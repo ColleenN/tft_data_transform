3967b31f60d8d795bf983fdc348d5b7c27686556
CNelson <colleenfnelson@gmail.com>
2025-03-23 16:38:22 -0600

    Updates to typing, base unit annotations

---
+++ models/match_detail/annotated/annotated_unit_entries.sql
@@ -0,0 +1,31 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+WITH initial_pass as
+(
+	SELECT *,
+	CASE
+		WHEN raw_unit_rarity < 3 THEN raw_unit_rarity+1
+		WHEN raw_unit_rarity = 6 THEN 5
+		ELSE raw_unit_rarity
+	END as true_unit_cost,
+	CASE
+		WHEN raw_unit_rarity < 7 THEN 1
+		ELSE 0
+	END as shop_unit
+	FROM {{ ref('board_unit_data') }}
+)
+SELECT
+    match_id, puuid, character_id, tier, raw_unit_rarity, item_json,
+    true_unit_cost, shop_unit,
+	CASE
+		WHEN shop_unit = 1 THEN true_unit_cost * (3 ^ (tier-1))
+		ELSE 0
+	END as gold_value,
+	jsonb_array_length(item_json) as num_items
+FROM initial_pass
+
+
--- models/match_detail/unpacked_json/board_unit_data.sql
+++ models/match_detail/unpacked_json/board_unit_data.sql
@@ -3,7 +3,7 @@ SELECT
 	puuid,
 	jsonb_array_elements(unit_list)->>'character_id' as character_id,
 	(jsonb_array_elements(unit_list)->>'tier')::int as tier,
-	jsonb_array_elements(unit_list)->>'raw_rarity' as unit_cost,
+	(jsonb_array_elements(unit_list)->>'rarity')::int as raw_unit_rarity,
 	jsonb_array_elements(unit_list)->'itemNames' as item_json,
 	jsonb_array_elements(unit_list) as raw_unit_json
 FROM {{ ref('base_board_data') }}

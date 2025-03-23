a0fbc26e27045f68e6f6b63b24ca6b1e81be7ac1
CNelson <colleenfnelson@gmail.com>
2025-03-23 15:21:07 -0600

    More accurate column name, why Riot WHY?

--- models/match_detail/unpacked_json/board_unit_data.sql
+++ models/match_detail/unpacked_json/board_unit_data.sql
@@ -2,8 +2,8 @@ SELECT
 	match_id,
 	puuid,
 	jsonb_array_elements(unit_list)->>'character_id' as character_id,
-	jsonb_array_elements(unit_list)->>'tier' as tier,
-	jsonb_array_elements(unit_list)->>'rarity' as unit_cost,
+	(jsonb_array_elements(unit_list)->>'tier')::int as tier,
+	jsonb_array_elements(unit_list)->>'raw_rarity' as unit_cost,
 	jsonb_array_elements(unit_list)->'itemNames' as item_json,
 	jsonb_array_elements(unit_list) as raw_unit_json
 FROM {{ ref('base_board_data') }}

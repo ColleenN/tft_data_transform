9c4d1eb37db4123e333dfa4b328e11c6ad822f8f
CNelson <colleenfnelson@gmail.com>
2025-03-25 20:10:08 -0600

    Annotated items

---
+++ models/match_detail/annotated/annotated_unit_item_entries.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+SELECT
+	match_id,
+	puuid,
+	character_id,
+	tier,
+	unit_instance_index,
+	seed_items.*
+FROM
+				{{ ref('board_unit_item_data') }}
+	LEFT JOIN 	{{ ref('seed_items') }}
+		ON item_api_name = api_name
+WHERE name IS NOT NULL

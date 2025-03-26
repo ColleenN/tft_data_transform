b4848b880b2589e66f57125f058e85bd66c9193e
CNelson <colleenfnelson@gmail.com>
2025-03-25 20:00:24 -0600

    Add indexing to items to account for duplicate items

--- models/match_detail/unpacked_json/board_unit_item_data.sql
+++ models/match_detail/unpacked_json/board_unit_item_data.sql
@@ -1,8 +1,30 @@
-SELECT
-	match_id,
-	puuid,
-	character_id,
-	tier,
-	unit_instance_index,
-	jsonb_array_elements_text(item_json) as item_api_name
-FROM {{ ref('board_unit_data') }}
+WITH initial AS (
+    SELECT
+	    match_id,
+        puuid,
+        character_id,
+        tier,
+        unit_instance_index,
+        jsonb_array_elements_text(item_json) as item_api_name
+    FROM {{ ref('board_unit_data') }}
+)
+
+SELECT  *,
+    row_number() OVER (
+        PARTITION BY
+            match_id,
+            puuid,
+            character_id,
+            tier,
+            unit_instance_index,
+            item_api_name
+    )
+    AS item_instance_index
+FROM initial
+
+
+
+
+
+
+--

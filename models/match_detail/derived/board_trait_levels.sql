dbe9e9707c053ce5aa714b760c9665caae233826
CNelson <colleenfnelson@gmail.com>
2025-03-29 23:19:11 -0600

    Re-structure to incorporate traits missing from top-line, for ghost unit detection

--- models/match_detail/derived/board_trait_levels.sql
+++ models/match_detail/derived/board_trait_levels.sql
@@ -3,15 +3,21 @@ WITH unit_traits AS
 	SELECT match_id, puuid, trait, COUNT(*) AS num_units_in_record
 	FROM {{ ref('unit_trait_contributions') }}
 	GROUP BY match_id, puuid, trait
-)
-
-SELECT 	active.match_id, active.puuid, trait,
-		active.tier_current tier_active,
-		active.num_units active_count, counted.num_units_in_record on_board_count,
-		active.num_units-counted.num_units_in_record AS raw_diff
-FROM
-				{{ ref('board_active_trait_data') }} active
-	LEFT JOIN 	unit_traits counted
+),
+matched_to_active AS
+(
+	SELECT 	counted.match_id, counted.puuid, trait,
+		CASE WHEN active.tier_current IS NULL THEN 0 ELSE active.tier_current END tier_active,
+		CASE WHEN active.num_units  IS NULL THEN 0 ELSE active.num_units END active_count,
+		counted.num_units_in_record on_board_count
+	FROM
+				unit_traits counted
+	LEFT JOIN 	{{ ref('board_active_trait_data') }} active
 		ON active.match_id = counted.match_id
 		AND active.puuid = counted.puuid
 		AND active.name = counted.trait
+)
+SELECT
+	*,
+	active_count - on_board_count AS raw_diff
+FROM matched_to_active

348a15ecb68c35a1ea675f1af7d51783b449d40e
CNelson <colleenfnelson@gmail.com>
2025-04-05 16:10:15 -0600

    Filter out non-ranked games

--- models/match_detail/unpacked_json/unique_raw_match_detail.sql
+++ models/match_detail/unpacked_json/unique_raw_match_detail.sql
@@ -7,17 +7,22 @@
 
 WITH all_tables AS
 (
-	SELECT metadata->>'match_id' as match_id, metadata, info,
-    'tft_player_match_detail' AS src_table, _sdc_sequence AS singer_id
+	SELECT metadata->>'match_id' AS match_id, metadata, info,
+        'tft_player_match_detail' AS src_table, _sdc_sequence AS singer_id,
+        (info->>'queue_id')::int AS queue_id
 	FROM tap_riotapi.tft_player_match_detail
 	UNION
-	SELECT metadata->>'match_id' as match_id, metadata, info,
-	    'apex_ranked_ladder_match_detail' AS src_table, _sdc_sequence AS singer_id
+	SELECT metadata->>'match_id' AS match_id, metadata, info,
+	    'apex_ranked_ladder_match_detail' AS src_table, _sdc_sequence AS singer_id,
+	    (info->>'queue_id')::int AS queue_id
 	FROM tap_riotapi.apex_ranked_ladder_match_detail
 	UNION
-	SELECT metadata->>'match_id' as match_id, metadata, info,
-	    'normal_ranked_ladder_match_detail' AS src_table, _sdc_sequence AS singer_id
+	SELECT metadata->>'match_id' AS match_id, metadata, info,
+	    'normal_ranked_ladder_match_detail' AS src_table, _sdc_sequence AS singer_id,
+	    (info->>'queue_id')::int AS queue_id
 	FROM tap_riotapi.normal_ranked_ladder_match_detail
 )
 
-SELECT DISTINCT ON(match_id) match_id, metadata, info, src_table, singer_id FROM all_tables
+SELECT DISTINCT ON(match_id) match_id, metadata, info, src_table, singer_id
+FROM all_tables
+WHERE queue_id = 1100

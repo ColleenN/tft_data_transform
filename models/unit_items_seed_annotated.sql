373a4822bf84a3145b7a38c391673b9a677635f5
colleenfnelson@gmail.com <colleenfnelson@gmail.com>
2025-11-09 03:20:13 +0000

    First seed annotation views

---
+++ models/unit_items_seed_annotated.sql
@@ -0,0 +1,30 @@
+WITH initial AS
+(
+	SELECT
+		match_id,
+		puuid,
+		character_id,
+		tier,
+		CASE WHEN SUM(num_tg_items
+		) OVER (
+			PARTITION BY match_id,
+			puuid,
+			character_id,
+			tier, unit_instance_index
+		) > 0 THEN True ELSE False END
+		AS has_generated_items,
+		unit_instance_index,
+		item_instance_index,
+		seed_items.*
+	FROM
+				    {{ ref('unpacked_unified') }}
+	    LEFT JOIN 	{{ ref('seed_items') }}
+		    ON item_api_name = api_name
+	WHERE name IS NOT NULL
+)
+
+SELECT *
+FROM initial
+WHERE
+    has_generated_items = False
+    OR (has_generated_items = True AND num_tg_items = 1)
---
+++ models/units_seed_annnotated.sql
@@ -0,0 +1,8 @@
+SELECT 
+    match_id, puuid, character_id, tier, unit_instance_index,
+    cost, role, shop_unit,
+    CAST(cost*POWER(3, tier-1) AS INT64) AS total_unit_cost
+FROM
+                {{ ref('unpacked_unified') }}
+    LEFT JOIN 	{{ ref('seed_units') }}
+        ON character_id = api_name

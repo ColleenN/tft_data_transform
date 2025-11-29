781102edae2d3e6442b01167da5b32c4a1bacb83
Colleen Nelson <colleenfnelson@gmail.com>
2025-11-29 19:46:42 +0000

    Initial agg work

---
+++ models/aggregations_units.sql
@@ -0,0 +1,7 @@
+SELECT  match_id, puuid, character_id, tier, unit_instance_index,
+        CASE WHEN num_tg_items IS NULL THEN 0 ELSE num_tg_items END AS tg_item,
+        {{ select_item_category_sums() }},
+        {{ select_item_component_sums() }}
+--FROM {{ ref('seed_annotated_units') }}
+FROM {{ ref('seed_annotated_unified') }}
+GROUP BY match_id, puuid, character_id, tier, unit_instance_index, num_tg_items
--- models/annotated/seed_annotated_units.sql
+++ models/annotated/seed_annotated_units.sql
@@ -1,7 +1,10 @@
 SELECT 
     match_id, puuid, character_id, tier, unit_instance_index,
     cost, role, shop_unit,
-    CAST(cost*POWER(3, tier-1) AS INT64) AS total_unit_cost
+    CASE 
+        WHEN shop_unit = True THEN CAST(cost*POWER(3, tier-1) AS INT64)
+        ELSE 0
+    END AS total_unit_cost
 FROM
                 {{ ref('unpacked_unified') }}
     LEFT JOIN 	{{ ref('seed_units') }}

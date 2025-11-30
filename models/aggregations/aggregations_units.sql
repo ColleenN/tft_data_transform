47a0d554fcc35e6388cd4f12aa25bb2b1c639484
Colleen Nelson <colleenfnelson@gmail.com>
2025-11-30 05:30:10 +0000

    Naming fix, first agg model

--- models/aggregations/aggregations_units.sql
+++ models/aggregations/aggregations_units.sql
@@ -1,7 +1,7 @@
 SELECT  match_id, puuid, character_id, tier, unit_instance_index,
         CASE WHEN num_tg_items IS NULL THEN 0 ELSE num_tg_items END AS tg_item,
+        COUNT(item_api_name) AS num_items,
         {{ select_item_category_sums() }},
         {{ select_item_component_sums() }}
---FROM {{ ref('seed_annotated_units') }}
 FROM {{ ref('seed_annotated_unified') }}
 GROUP BY match_id, puuid, character_id, tier, unit_instance_index, num_tg_items

824ec90294240f6f6bed9588033124ee6c7160f2
Colleen Nelson <colleenfnelson@gmail.com>
2025-11-30 05:13:09 +0000

    Carve-out for 7 cost units

--- models/annotated/seed_annotated_units.sql
+++ models/annotated/seed_annotated_units.sql
@@ -1,10 +1,17 @@
 SELECT 
     match_id, puuid, character_id, tier, unit_instance_index,
-    cost, role, shop_unit,
+    cost AS rarity, 
     CASE 
+        WHEN character_id IN ('TFT16_AURELIONSOL', 'TFT16_BROCK', 'TFT16_SYLAS', 'TFT16_RYZE', 'TFT16_BARONNASHOR', 'TFT16_ZAAHEN') THEN 7
+        ELSE cost
+    END AS shop_gold_cost,
+    role, shop_unit,
+    CASE 
+        WHEN character_id IN ('TFT16_AURELIONSOL', 'TFT16_BROCK', 'TFT16_SYLAS', 'TFT16_RYZE', 'TFT16_BARONNASHOR', 'TFT16_ZAAHEN') THEN CAST(7*POWER(3, tier-1) AS INT64)
         WHEN shop_unit = True THEN CAST(cost*POWER(3, tier-1) AS INT64)
         ELSE 0
     END AS total_unit_cost
+
 FROM
                 {{ ref('unpacked_unified') }}
     LEFT JOIN 	{{ ref('seed_units') }}

87a4856e63ae7de1e753afe5474347dc59a237a9
Colleen Nelson <colleenfnelson@gmail.com>
2025-12-03 05:50:07 +0000

    patch classification

--- models/staging/unique_matches.sql
+++ models/staging/unique_matches.sql
@@ -12,6 +12,7 @@ WITH all_tables AS
         JSON_QUERY(data, "$.metadata") AS metadata,
         JSON_QUERY(data, "$.info") AS info,
         JSON_VALUE(data, "$.info.queue_id") AS queue_id,
+        CAST(TIMESTAMP_MILLIS(CAST(JSON_VALUE(data, "$.info.gameCreation") AS INT64)) AS DATETIME) AS server_side_game_start,
         REGEXP_EXTRACT(JSON_VALUE(data, "$.info.game_version"), r'[^\(]*?\(([^\)]*?)\)') AS last_client_update_datetime,
         REGEXP_EXTRACT(JSON_VALUE(data, "$.info.game_version"), r'Linux Version (.*?) \(' ) AS game_client_version_id,
         CAST(JSON_VALUE(data, "$.info.tft_set_number") AS INT64) AS tft_set_number,
@@ -23,6 +24,7 @@ WITH all_tables AS
         JSON_QUERY(data, "$.metadata") AS metadata,
         JSON_QUERY(data, "$.info") AS info,
         JSON_VALUE(data, "$.info.queue_id") AS queue_id,
+        CAST(TIMESTAMP_MILLIS(CAST(JSON_VALUE(data, "$.info.gameCreation") AS INT64)) AS DATETIME) AS server_side_game_start,
         REGEXP_EXTRACT(JSON_VALUE(data, "$.info.game_version"), r'[^\(]*?\(([^\)]*?)\)') AS last_client_update_datetime,
         REGEXP_EXTRACT(JSON_VALUE(data, "$.info.game_version"), r'Linux Version (.*?) \(' ) AS game_client_version_id,
         CAST(JSON_VALUE(data, "$.info.tft_set_number") AS INT64) AS tft_set_number,
@@ -30,8 +32,9 @@ WITH all_tables AS
 	FROM match_data_landing_zone.tft_player_match_detail
 )
 
-SELECT  match_id, SPLIT(match_id, '_')[0] AS server_realm_code, queue_id, tft_set_number,
-        metadata, info, src_table, singer_id, last_client_update_datetime, game_client_version_id,
+SELECT  match_id, SPLIT(match_id, '_')[0] AS server_realm_code, queue_id, server_side_game_start, tft_set_number,
+        metadata, info, src_table, singer_id, last_client_update_datetime, 
+        game_client_version_id,
 ROW_NUMBER() OVER (PARTITION BY match_id ORDER BY singer_id DESC) AS instance
 FROM all_tables
 WHERE ((SPLIT(match_id, '_')[0] = 'PBE1' and queue_id = '1090') OR (queue_id = '1100'))
